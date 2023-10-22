dba0c3af48cb8323e78f9bb9dc8417cb3c05751b
Adam Hofman <68481457+adamhoof@users.noreply.github.com>
2023-10-22 20:41:09 +0200

    Update Low_Tier_Code.ino
    Initial library version

--- Low_Tier_Code.ino
+++ Low_Tier_Code.ino
@@ -1,45 +1,3 @@
-
-/*
-* Original sourse: https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
-* This is the Arduino code PAC6985 16 channel servo controller
-* watch the video for details and demo http://youtu.be/y8X9X10Tn1k
-*  *
-
-* Watch video for this code:
-*
-* Related Videos
-V5 video of PCA9685 32 Servo with ESP32 with WiFi https://youtu.be/bvqfv-FrrLM
-V4 video of PCA9685 32 Servo with ESP32 (no WiFi): https://youtu.be/JFdXB8Za5Os
-V3 video of PCA9685 how to control 32 Servo motors https://youtu.be/6P21wG7N6t4
-V2 Video of PCA9685 3 different ways to control Servo motors: https://youtu.be/bal2STaoQ1M
-V1 Video introduction to PCA9685 to control 16 Servo  https://youtu.be/y8X9X10Tn1k
-
-* Written by Ahmad Shamshiri for Robojax Video channel www.Robojax.com
-* Date: Dec 16, 2017, in Ajax, Ontario, Canada
-* Permission granted to share this code given that this
-* note is kept with the code.
-* Disclaimer: this code is "AS IS" and for educational purpose only.
-* this code has been downloaded from http://robojax.com/learn/arduino/
-*
-*/
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include "Dictionary.h"
@@ -47,10 +5,34 @@ V1 Video introduction to PCA9685 to control 16 Servo  https://youtu.be/y8X9X10Tn
 class Servo
 {
 public:
-    explicit Servo(const String& n, uint16_t minAng, uint16_t maxAng) : name(n), minAngle(minAng), maxAngle(maxAng)
+    explicit Servo(const String& name, uint8_t index, uint16_t minAng, uint16_t maxAng) : name(name), index(index),
+                                                                                          minAngle(minAng),
+                                                                                          maxAngle(maxAng)
     {};
+
+    const String& getName() const
+    {
+        return this->name;
+    }
+
+    uint8_t getIndex() const
+    {
+        return this->index;
+    }
+
+    uint8_t getMinAngle() const
+    {
+        return this->minAngle;
+    }
+
+    uint8_t getMaxAngle() const
+    {
+        return this->maxAngle;
+    }
+
 private:
     String name;
+    uint8_t index {0};
     uint16_t minAngle {0};
     uint16_t maxAngle {180};
 };
@@ -67,23 +49,33 @@ public:
         driver.setPWMFreq(this->servoFrequency);
     }
 
-    void setServoFrequency(uint8_t frequency)
+    void setDriverFrequency(uint8_t frequency)
     {
         this->servoFrequency = frequency;
     }
 
-    void addServo(uint8_t servoIndex, Servo servo)
+    void addServo(const Servo& servo)
     {
-        servoMap.set(servoIndex, servo);
+        if (servosMap.contains(servo.getName())){
+            return;
+        }
+
+        servosMap.set(servo.getName(), servo);
     }
 
     void moveServo(const String& servoName, uint16_t angle)
     {
+        Servo servo = servosMap.get(servoName);
 
+        if (angle > servo.getMaxAngle() || angle < servo.getMinAngle()) {
+            return;
+        }
+
+        driver.setPWM(servo.getIndex(), 0, angleToPulse(servo.getMinAngle(), servo.getMaxAngle(), angle));
     }
 
 private:
-    Dictionary<uint8_t , Servo> servoMap;
+    Dictionary<String, Servo> servosMap;
     uint8_t servoFrequency {60};
     uint16_t minPulseLength {150};
     uint16_t maxPulseLength {600};
@@ -103,28 +95,18 @@ ServoDriver servoDriver;
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("16 channel Servo test!");
-
     servoDriver.setup();
-
-    //yield();
+    yield();
 }
 
-// the code inside loop() has been updated by Robojax
 void loop()
 {
+    servoDriver.addServo(Servo("xaxis", 0, 0, 180));
+    servoDriver.addServo(Servo("yaxis", 1, 0, 180));
 
-
-    servoDriver.addServo(0, Servo("xaxis", 0,180));
-    servoDriver.addServo(0, Servo("yaxis", 0,180));
-
-    servoDriver.moveServo()
-    for (int angle = 0; angle < 181; angle += 20) {
-        delay(500);
-        sd.setPWM(0, 0, angleToPulse(angle));
+    for (uint16_t angle = 0; angle <= 180; angle++) {
+        delay(30);
+        servoDriver.moveServo("xaxis", angle);
+        servoDriver.moveServo("yaxis", angle);
     }
-
-    delay(1000);
-
 }
